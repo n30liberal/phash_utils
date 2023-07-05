b07bd62fe2eef4df8f2f9d7cde52d1248a3bfa11
n30liberal <118079899+n30liberal@users.noreply.github.com>
2023-07-05 04:19:52 -0500

    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -1,8 +1,5 @@
 from pathlib import Path
 
-flask_server = "http://127.0.0.1:5000"
-stash_server = "http://localhost:9999"
-
 # path to your stash database [we're only reading from it, but it's still a good idea to make a backup]
 stash_database_path = Path("path/to/stash/database.sqlite")
 
@@ -18,9 +15,10 @@ processed_images_path = data_directory / "phashed_file_ids.txt"
 direct_delete = False
 trash_directory = Path("path/to/trash/directory") # folder you dedicate to trash, so you can easily restore stuff
 collections_directory = Path("path/to/your/isos") # main root folder for your collections
+extracted_frames_path = Path("path/to/your/extracted/frames") # this dir gets deleted each call
 
 auto_delete = False
-output_to_flask = False
+output_to_window = False
 
 mse_image_threshold = 40
 mse_video_threshold = 40
@@ -30,6 +28,7 @@ allowed_media_types = ["video"]
 min_group_size = 0 * 1024 * 1024
 min_group_duration = 0
 whitelist_models = []
+blacklist_models = []
 
 # so we know which media_type to use for a file
 image_extensions = [".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".webp", ".jfif"]
