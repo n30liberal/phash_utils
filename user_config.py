eec7ec8a9e9c12adff2cbc7a18551282a1824fe0
n30liberal <118079899+n30liberal@users.noreply.github.com>
2023-07-06 02:30:59 -0500

    Update user_config.py

--- user_config.py
+++ user_config.py
@@ -1,11 +1,33 @@
 from pathlib import Path
 
+
+def readable_size(bytes):
+    if bytes < 1024:
+        return f"{bytes} B"
+    elif bytes < 1024**2:
+        return f"{bytes / 1024:.2f} KB"
+    elif bytes < 1024**3:
+        return f"{bytes / 1024 ** 2:.2f} MB"
+    elif bytes < 1024**4:
+        return f"{bytes / 1024 ** 3:.2f} GB"
+    else:
+        return f"{bytes / 1024 ** 4:.2f} TB"
+
+
+def readable_duration(float):
+    seconds = int(float)
+
+    hours = seconds // 3600
+    minutes = (seconds % 3600) // 60
+    seconds = seconds % 60
+
+    return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+
+
 # path to your stash database [we're only reading from it, but it's still a good idea to make a backup]
 stash_database_path = Path("path/to/stash/database.sqlite")
 
 # paths to all the files/dirs we're going to be using
-# i want to move the imported py files (build_db, this config, companion_server, generate_missing_..., ) into a new "dependencies" folder
-# will do that later
 data_directory = Path(__file__).parent / "data"
 blacklisted_phash_path = data_directory / "blacklisted_phashes.txt"
 database_path = data_directory / "stash_data.sqlite"
@@ -23,14 +45,12 @@ output_to_window = False
 mse_image_threshold = 40
 mse_video_threshold = 40
 
-# for filtering groups
 allowed_media_types = ["video"]
 min_group_size = 0 * 1024 * 1024
 min_group_duration = 0
 whitelist_models = []
 blacklist_models = []
 
-# so we know which media_type to use for a file
 image_extensions = [".jpg", ".jpeg", ".png", ".bmp", ".tiff", ".webp", ".jfif"]
 video_extensions = [
     ".mp4",
